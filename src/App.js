6cf6ee52af9bf2ae3867e6107ca03f04ea8b6b8b
cwooz <woosley.christopher@gmail.com>
2018-08-10 23:46:00 -0400

    contact component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,12 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="666-555-5555"
-        />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
     );
   }
