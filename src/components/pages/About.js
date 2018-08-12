e80cac7b57adba0830c3e4db1064fce314b17687
cwooz <woosley.christopher@gmail.com>
2018-08-12 01:09:47 -0400

    addLink

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,9 @@ export default () => {
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple app to manage contacts.</p>
       <p className="text-secondary">Version 1.0.0</p>
+      <a href="https://github.com/cwooz/reactContactManager" target="_blank">
+        https://github.com/cwooz/reactContactManager
+      </a>
     </div>
   );
 };
